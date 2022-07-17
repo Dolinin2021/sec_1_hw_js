a958c80848ca3b1a42d8d7cb5270cd9c19fbf5b6
Dolinin2021 <programmistdolinin@mail.ru>
2022-07-17 13:50:33 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let n = Math.floor(Math.random() * 1000);
+console.log(n);
+
+while(true) {
+    const userGuess = prompt('Я загадал число в диапазоне от 0 до 999, попробуйте угадать:');
+
+    if (userGuess == 'q') {
+        break;
+    }
+    else if (isNaN(userGuess)) {
+        alert('Вы ввели некорректное значение');
+    }
+    else if (+userGuess === n) {
+        alert('Вы угадали!');
+        break;
+    } else {
+        alert('Вы не угадали!');
+    }
+}
